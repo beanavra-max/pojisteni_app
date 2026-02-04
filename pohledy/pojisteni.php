e7797bbb0c6d04655a69242fea42015d7d604cf2
Bea-Navra <beanavra@gmail.com>
2026-02-04 02:06:08 +0100

    Fix avatar image source path in pojisteni.php

--- pohledy/pojisteni.php
+++ pohledy/pojisteni.php
@@ -35,7 +35,7 @@
 <form method="post" class="novy-pojistenec-form">
 
     <div class="avatar">
-        <img src="avatar.png" alt="Avatar" />
+        <img src="/pojisteni_app/avatar.png" alt="Avatar" />
     </div>
 
     <div class="form-right-side">
@@ -66,4 +66,5 @@
 
 </body>
 </html>
+
 
